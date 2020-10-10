244d5c37a2eaa888dac571441c4ea1769a54bb45
Jan Skála <j.skala@hotmail.com>
2020-10-10 12:38:34 +0200

    Rescaffold client.

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -28,26 +28,10 @@
       </li>
       <li>
         <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex"
-          target="_blank"
-          rel="noopener"
-          >vuex</a
-        >
-      </li>
-      <li>
-        <a
           href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
           target="_blank"
           rel="noopener"
           >eslint</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript"
-          target="_blank"
-          rel="noopener"
-          >typescript</a
         >
       </li>
     </ul>
@@ -110,13 +94,13 @@
   </div>
 </template>
 
-<script lang="ts">
-import { Component, Prop, Vue } from "vue-property-decorator";
-
-@Component
-export default class HelloWorld extends Vue {
-  @Prop() private msg!: string;
-}
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  }
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
--- client/src/main.js
+++ client/src/main.js
@@ -1,12 +1,15 @@
 import Vue from "vue";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 import App from "./App.vue";
 import router from "./router";
-import store from "./store";
 
 Vue.config.productionTip = false;
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
 
 new Vue({
   router,
-  store,
   render: h => h(App)
 }).$mount("#app");
