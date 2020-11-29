57365471cd18f4413a1fef9788b58f9de8f1f3e8
Jan Skála <j.skala@hotmail.com>
2020-11-29 10:34:36 +0100

    Try router history mode.

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <div>Token: {{ token }}</div>
-    <b-form @submit="onSubmit" v-if="!token">
+    <div>Code: {{ code }}</div>
+    <b-form @submit="onSubmit" v-if="!code">
       <b-button type="submit" variant="primary">Login with CVUT</b-button>
     </b-form>
   </div>
@@ -10,22 +10,22 @@
 <script>
 export default {
   computed: {
-    token: function() {
+    code: function() {
       return this.$route.query.code;
     }
   },
   methods: {
     onSubmit(evt) {
       evt.preventDefault();
-      //const redirect = location.href;
-      const redirect = "https://enjqvdake9lq.x.pipedream.net";
+      const redirect = location.href.replace("/#/","");
+      //const redirect = "https://enjqvdake9lq.x.pipedream.net";
       const clientId = process.env.VUE_APP_CLIENT_ID;
       window.location.href = `https://auth.fit.cvut.cz/oauth/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirect}`;
     }
   },
   mounted: function() {
       if(this.$route.query.code){
-          // TODO Get token from API
+          // TODO Get Token from API
       }
   }
 };
