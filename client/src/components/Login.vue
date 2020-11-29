75cb6e6baaa1c5f0a16e138b6308ce2a5a1a3532
Jan Skála <j.skala@hotmail.com>
2020-11-29 20:13:20 +0100

    Parse oAuth2 code from query.

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -11,22 +11,29 @@
 export default {
   computed: {
     code: function() {
+      // HACK parse query parameter even from github pages
+      const regex = /(?<=code=)(\w*)(?=(&|#)?)/gm;
+      const found = window.location.href.match(regex);
+      if (found && found.length == 1) {
+        return found[0];
+      }
       return this.$route.query.code;
-    }
+    },
   },
   methods: {
     onSubmit(evt) {
       evt.preventDefault();
-      const redirect = location.href.replace("/#/","");
-      //const redirect = "https://enjqvdake9lq.x.pipedream.net";
+      const redirect = location.href.replace("/#/", "");
       const clientId = process.env.VUE_APP_CLIENT_ID;
       window.location.href = `https://auth.fit.cvut.cz/oauth/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirect}`;
-    }
+    },
   },
   mounted: function() {
-      if(this.$route.query.code){
-          // TODO Get Token from API
-      }
-  }
+    alert(this.code());
+    if (this.code()) {
+      // TODO Get Token from API
+      alert(this.code());
+    }
+  },
 };
 </script>
