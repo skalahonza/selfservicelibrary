3ed5babf39bb5dabee7e920926efe452170fe556
Jan Skála <j.skala@hotmail.com>
2020-12-01 10:03:27 +0100

    Landing page.

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -1,11 +1,42 @@
 <template>
   <div>
-    <div v-if="authorized">
-      Signed in as {{user.fullName}}
-    </div>
-    <b-form @submit="onSubmit" v-else>
-      <b-button type="submit" variant="primary">Login with CVUT</b-button>
-    </b-form>
+    <b-jumbotron>
+      <template #header>Self Service Library</template>
+
+      <template #lead>
+        Online Self service library for university departments.
+      </template>
+
+      <hr class="my-4" />
+
+      <div v-if="authorized">
+        <p>Signed in as {{ user.fullName }}</p>
+        <b-button @click="$router.push('books')" pill variant="primary">View books</b-button>
+        <b-button @click="$router.push('my-issues')" pill variant="success">View issues</b-button>
+      </div>
+
+      <b-form @submit="onSubmit" v-else>
+        <p>
+          You can begin by logging in with your university credentials. We use
+          official
+          <b-link href="https://auth.fit.cvut.cz/" target="_blank"
+            >CVUT authorization server</b-link
+          >. We do not store your password or user info anywhere.
+        </p>
+        <b-overlay
+          :show="code"
+          rounded="sm"
+          bg-color="#e9ecef"
+          spinner-variant="primary"
+          spinner-type="grow"
+          spinner-small
+        >
+          <b-button type="submit" variant="primary" pill
+            >Login with CVUT</b-button
+          >
+        </b-overlay>
+      </b-form>
+    </b-jumbotron>
   </div>
 </template>
 
@@ -17,11 +48,11 @@ export default {
     authorized: function () {
       return isAuthorized();
     },
-    user: function() {
+    user: function () {
       return {
         preferredEmail: localStorage.getItem("preferredEmail"),
-        fullName: localStorage.getItem("fullName")
-      }
+        fullName: localStorage.getItem("fullName"),
+      };
     },
     code: function () {
       // HACK parse query parameter even from github pages
