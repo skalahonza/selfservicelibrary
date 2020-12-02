b50011d432b273b5b286482e1f9e069f1c75f2cb
Jan Skála <j.skala@hotmail.com>
2020-12-02 12:14:24 +0100

    Return a book.

--- client/src/views/MyIssues.vue
+++ client/src/views/MyIssues.vue
@@ -18,24 +18,57 @@
       <template #cell(isReturned)="row">
         <b-check v-model="row.item.isReturned" disabled />
       </template>
+      <template #cell(action)="row">
+        <b-button
+          variant="success"
+          :disabled="row.item.isReturned"
+          v-on:click="returnIssue(row.item)"
+          >Return</b-button
+        >
+      </template>
     </b-table>
   </div>
 </template>
 
 <script>
-import { getMine } from "@/services/issues";
-import moment from 'moment';
+import { getMine, returnIssue } from "@/services/issues";
+import moment from "moment";
 export default {
   name: "MyIssues",
-  async created() {
-    this.isBusy = true;
-    this.items = await getMine();
-    this.isBusy = false;
+  created() {
+    this.reload();
   },
   methods: {
+    async reload() {
+      this.isBusy = true;
+      const items = await getMine(); 
+      items.sort((a,b) =>{
+        return (a.isReturned - b.isReturned) || (a.expiryDate - b.expiryDate);
+      });
+      this.items = items;
+      this.isBusy = false;
+    },
+    returnIssue(item) {
+      returnIssue(item.id)
+        .then(() => {
+          this.$bvToast.toast("Book successfully returned.", {
+            title: "Success",
+            variant: "success",
+            solid: true,
+          });
+          this.reload();
+        })
+        .catch((error) => {
+          this.$bvToast.toast(error.response.data.title, {
+            title: "Chyba",
+            variant: "danger",
+            solid: true,
+          });
+        });
+    },
     formatDateAssigned(value) {
       if (value) {
-        return moment(value).format('LL');
+        return moment(value).format("LL");
       }
       return value;
     },
@@ -76,6 +109,7 @@ export default {
           label: "Reutrned",
           sortable: true,
         },
+        { key: "action" },
       ],
       items: [],
       currentPage: 0,
