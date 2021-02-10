de48c1a0f6b3421bd2172651ab624bf046b163dd
Jan Skala <skalaja7@fel.cvut.cz>
2021-02-10 19:15:44 +0100

    Remove enforced HTTPS.

--- server/SelfServiceLibrary.Web/Startup.cs
+++ server/SelfServiceLibrary.Web/Startup.cs
@@ -41,15 +41,10 @@ namespace SelfServiceLibrary.Web
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
-
             app.UseRouting();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapBlazorHub();
