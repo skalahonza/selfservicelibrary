5e790355e3db576584458fbb9f2deae3835c7194
Jan Skala <skalaja7@fel.cvut.cz>
2021-02-15 21:35:47 +0100

    Remove old client code.

--- client/src/components/ScanQR.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div>
-    <p class="my-4">Scanning book QR Code.</p>
-    <b-progress :value="100" variant="success" striped :animated="true" />
-  </div>
-</template>
--- client/src/services/api.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-    baseURL: process.env.VUE_APP_API_URL,
-    headers: {
-        "Accept": "application/json",
-        "Content-Type": "application/json"
-    }
-})
