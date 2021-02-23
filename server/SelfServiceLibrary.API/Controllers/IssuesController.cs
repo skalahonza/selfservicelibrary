8a849fd9b81c6c7918fe2e71bf0f061d33842777
Jan Skala <skalaja7@fel.cvut.cz>
2021-02-23 14:24:06 +0100

    Open API specification.

---
+++ server/SelfServiceLibrary.API/Controllers/IssuesController.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace SelfServiceLibrary.API.Controllers
+{
+    public class IssuesController : BaseController
+    {
+        /// <summary>
+        /// Borrow a book from a library
+        /// </summary>
+        /// <param name="departmentNumber">Department number of the book to be borrowed.</param>
+        /// <returns></returns>
+        [HttpPost("{departmentNumber}/borrow")]
+        public async Task<IActionResult> Borrow(string departmentNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Return a previously borrowed book
+        /// </summary>
+        /// <param name="departmentNumber">Department number of the book to be returned.</param>
+        /// <returns></returns>
+        [HttpPost("{departmentNumber}/return")]
+        public async Task<IActionResult> Return(string departmentNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- server/SelfServiceLibrary.API/Startup.cs
+++ server/SelfServiceLibrary.API/Startup.cs
@@ -4,21 +4,22 @@ using System.Reflection;
 
 using AutoMapper;
 
-using CVUT.Auth;
-using CVUT.Auth.Options;
-using CVUT.Usermap;
-
 using FluentValidation.AspNetCore;
 
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
 
-using SelfServiceLibrary.API.Middlewares;
+using MongoDB.Driver;
+
+using SelfServiceLibrary.Mapping;
 using SelfServiceLibrary.Mapping.Profiles;
+using SelfServiceLibrary.Persistence.Options;
 using SelfServiceLibrary.Service.DTO.Book;
+using SelfServiceLibrary.Service.Services;
 using SelfServiceLibrary.Service.Validation;
 
 namespace SelfServiceLibrary.API
@@ -45,27 +46,6 @@ namespace SelfServiceLibrary.API
                     Description = "Self service library for university departments.",
                 });
 
-                var securitySchema = new OpenApiSecurityScheme
-                {
-                    Description = "Please enter your JWT token into field without Bearer prefix, e.g. 4b8afda3-e6d9-4109-aef5-d7d9993e1821",
-                    Type = SecuritySchemeType.Http,
-                    BearerFormat = "GUID",
-                    In = ParameterLocation.Header,
-                    Scheme = "bearer",
-                    Reference = new OpenApiReference
-                    {
-                        Type = ReferenceType.SecurityScheme,
-                        Id = "Bearer"
-                    }
-                };
-                c.AddSecurityDefinition("Bearer", securitySchema);
-                var securityRequirement = new OpenApiSecurityRequirement
-                {
-                    { securitySchema, new[] { "Bearer" } }
-                };
-                c.AddSecurityRequirement(securityRequirement);
-
-
                 var assemblies = new[] { Assembly.GetExecutingAssembly(), typeof(BookListDTO).Assembly };
                 foreach (var assembly in assemblies)
                 {
@@ -85,20 +65,28 @@ namespace SelfServiceLibrary.API
                 .AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<BookAddDTOValidator>())
                 .AddNewtonsoftJson();
 
-            services.AddOptions<oAuth2Options>().Bind(Configuration.GetSection("oAuth2")).ValidateDataAnnotations();
-            services.AddHttpContextAccessor();
-            services.AddHttpClient();
-            services.AddHttpClient<UsermapClient>();
-            services.AddHttpClient<ZuulClient>();
+            // Business logic
+            services.AddScoped<IssueService>();
 
             // Mapping
-            services.AddAutoMapper(typeof(IssueProfile));
+            services.AddAutoMapper(typeof(BookProfile));
+            services.AddScoped<Service.Interfaces.IMapper, AutoMapperAdapter>();
+
+            // Persistence, MongoDB
+            services
+                .AddOptions<MongoDbOptions>()
+                .Bind(Configuration.GetSection("MongoDb"))
+                .ValidateDataAnnotations();
+            services.AddSingleton<IMongoClient, MongoClient>(x =>
+            {
+                var options = x.GetRequiredService<IOptions<MongoDbOptions>>();
+                return new MongoClient(options.Value.ConnectionString);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseErrorHandlingMiddleware();
             ConfigureSwagger(app);
 
             app.UseRouting();
