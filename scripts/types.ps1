355cfe60dc75e56b28e7cc9ff75eb147f5c42283
Jan Skala <skalaja7@fel.cvut.cz>
2021-03-10 13:10:05 +0100

    Update csvs and powershell scripts.

--- scripts/types.ps1
+++ scripts/types.ps1
@@ -1,2 +1,2 @@
 $csvFile = Get-ChildItem -Path "..\data\*" -Include "*.csv"
-Import-Csv $csvFile -Header (1..31) -Delimiter ";" | Select-Object -ExpandProperty  '4' -Unique
+Import-Csv $csvFile -Header (1..31) -Delimiter ";" | Select-Object -ExpandProperty  '4' -Unique | Sort-Object
