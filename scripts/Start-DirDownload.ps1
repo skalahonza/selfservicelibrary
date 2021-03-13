3858d2d1cfa2b9fd316c4ce5880088a82a310576
Jan Skala <skalaja7@fel.cvut.cz>
2021-03-13 12:31:13 +0100

    Update csvs from server script.

---
+++ scripts/Start-DirDownload.ps1
@@ -0,0 +1,25 @@
+Param
+(
+    [parameter(Position=0, Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+    [String]$Downloadurl = 'http://cyber.felk.cvut.cz/novakpe/Skola/Knihovna/ToNewDB/',
+    [parameter(Position=1, Mandatory=$true,ValueFromPipelineByPropertyName=$true)]
+    [alias('DownloadPath')]
+    [String]$DownloadToFolder = 'C:\Temp\test'
+)
+
+function Combine-UriParts ($base, $path)
+{
+    return [Uri]::new([Uri]::new($base), $path).ToString()
+}
+
+$HttpContent = Invoke-WebRequest -URI $Downloadurl
+$csvs = $HttpContent.Links | Where-Object href -match "csv" | Select-Object -Expand href 
+
+$WebClient = New-Object System.Net.WebClient
+
+foreach ($file in $csvs) {    
+    $source = Combine-UriParts $Downloadurl $file
+    $destination = Join-Path $DownloadToFolder $file
+    Write-Host "Downloading $source to $destination"
+    $WebClient.DownloadFile($source, (Resolve-Path $destination))
+}
