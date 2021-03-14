feaf53893f96068389e06cf6d2d8805f3da69058
Jan Skala <skalaja7@fel.cvut.cz>
2021-03-14 09:25:33 +0100

    Add year,pages,type fields to book list.

--- server/SelfServiceLibrary.Service/DTO/Book/BookListDTO.cs
+++ server/SelfServiceLibrary.Service/DTO/Book/BookListDTO.cs
@@ -8,6 +8,9 @@ namespace SelfServiceLibrary.Service.DTO.Book
         public string? Name { get; set; }
         public string? PublicationType { get; set; }
         public string? Author { get; set; }
+        public int? YearOfPublication { get; set; }
+        public int? Pages { get; set; }
+        public string? FormType { get; set; }
         public List<string> Keywords { get; set; } = new List<string>();
         public bool IsAvailable { get; set; }
     }
