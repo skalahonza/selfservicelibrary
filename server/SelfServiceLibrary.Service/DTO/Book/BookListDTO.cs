ef1b4373d298707839107671c2f5b9977d71d4e2
Jan Skala <skalaja7@fel.cvut.cz>
2021-03-14 20:36:21 +0100

    Unauthorized column fields.

--- server/SelfServiceLibrary.Service/DTO/Book/BookListDTO.cs
+++ server/SelfServiceLibrary.Service/DTO/Book/BookListDTO.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace SelfServiceLibrary.Service.DTO.Book
 {
@@ -8,7 +9,10 @@ namespace SelfServiceLibrary.Service.DTO.Book
         public string? Name { get; set; }
         public string? PublicationType { get; set; }
         public string? Author { get; set; }
+        public List<string> CoAuthors { get; set; } = new List<string>();
+        public IEnumerable<string?> Authors => CoAuthors.Prepend(Author);
         public int? YearOfPublication { get; set; }
+        public int? Publication { get; set; }
         public int? Pages { get; set; }
         public string? FormType { get; set; }
         public List<string> Keywords { get; set; } = new List<string>();
