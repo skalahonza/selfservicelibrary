a70b5f95e83d1a04958ec3560934f6052f5930d7
Jan Skala <skalaja7@fel.cvut.cz>
2021-03-15 09:15:11 +0100

    Add librarian specific fields to book list.

--- server/SelfServiceLibrary.Service/DTO/Book/BookListDTO.cs
+++ server/SelfServiceLibrary.Service/DTO/Book/BookListDTO.cs
@@ -6,8 +6,12 @@ namespace SelfServiceLibrary.Service.DTO.Book
     public class BookListDTO
     {
         public string? DepartmentNumber { get; set; }
+        public string? SystemNumber { get; set; }
+        public string? FelNumber { get; set; }
+        public string? ISBNorISSN { get; set; }
         public string? Name { get; set; }
         public string? PublicationType { get; set; }
+        public string? Depended { get; set; }
         public string? Author { get; set; }
         public List<string> CoAuthors { get; set; } = new List<string>();
         public IEnumerable<string?> Authors => CoAuthors.Prepend(Author);
@@ -16,6 +20,8 @@ namespace SelfServiceLibrary.Service.DTO.Book
         public int? Pages { get; set; }
         public string? FormType { get; set; }
         public List<string> Keywords { get; set; } = new List<string>();
+        public string? Note { get; set; }
+        public string? StatusName { get; set; }
         public bool IsAvailable { get; set; }
     }
 }
---
+++ server/SelfServiceLibrary.Web/Extensions/StringExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace SelfServiceLibrary.Web.Extensions
+{
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Returns --- if the string is null or empty
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static string DashEmpty(this string text) =>
+            string.IsNullOrEmpty(text)
+            ? "---"
+            : text;
+    }
+}
