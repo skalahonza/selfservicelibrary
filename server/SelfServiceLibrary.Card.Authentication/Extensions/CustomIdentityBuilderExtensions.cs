7c7aa215fbaa36a6c70a4f5be3dfc2ac6bd88305
Jan Skala <skalaja7@fel.cvut.cz>
2021-03-18 12:05:20 +0100

    Enrich entropy with username of the card owner.

--- server/SelfServiceLibrary.Card.Authentication/Extensions/CustomIdentityBuilderExtensions.cs
+++ server/SelfServiceLibrary.Card.Authentication/Extensions/CustomIdentityBuilderExtensions.cs
@@ -5,12 +5,11 @@ using SelfServiceLibrary.Card.Authentication.Providers;
 
 namespace SelfServiceLibrary.Card.Authentication.Extensions
 {
-    public static class CustomIdentityBuilderExtensions
+    internal static class CustomIdentityBuilderExtensions
     {
-        public static IdentityBuilder AddCardLoginTokenProvider(this IdentityBuilder builder)
+        internal static IdentityBuilder AddCardLoginTokenProvider(this IdentityBuilder builder)
         {
-            var userType = builder.UserType;
-            var provider = typeof(CardLoginTokenProvider<>).MakeGenericType(userType);
+            var provider = typeof(CardLoginTokenProvider);
             return builder.AddTokenProvider(CardLoginTokenProvider.NAME, provider);
         }
     }
--- server/SelfServiceLibrary.Card.Authentication/Providers/CardLoginTokenProvider.cs
+++ server/SelfServiceLibrary.Card.Authentication/Providers/CardLoginTokenProvider.cs
@@ -3,17 +3,29 @@ using System.Threading.Tasks;
 
 using Microsoft.AspNetCore.Identity;
 
+using SelfServiceLibrary.Card.Authentication.Model;
+
 namespace SelfServiceLibrary.Card.Authentication.Providers
 {
-    public static class CardLoginTokenProvider
+    public class CardLoginTokenProvider : TotpSecurityStampBasedTokenProvider<IdCard>        
     {
         public const string NAME = "CardLoginTokenProvider";
-    }
 
-    public class CardLoginTokenProvider<TUser> : TotpSecurityStampBasedTokenProvider<TUser>
-        where TUser : class
-    {
-        public override Task<bool> CanGenerateTwoFactorTokenAsync(UserManager<TUser> manager, TUser user) =>
+        public override Task<bool> CanGenerateTwoFactorTokenAsync(UserManager<IdCard> manager, IdCard user) =>
             Task.FromResult(false);
+
+        /// <summary>
+        /// Returns a constant, provider and user unique modifier used for entropy in generated tokens from user information.
+        /// </summary>
+        /// <param name="purpose">The purpose the token will be generated for.</param>
+        /// <param name="manager">The <see cref="UserManager{TUser}" /> that can be used to retrieve user properties.</param>
+        /// <param name="user">The user a token should be generated for.</param>
+        /// <returns>
+        /// The <see cref="Task" /> that represents the asynchronous operation, containing a constant modifier for the specified 
+        /// <paramref name="user" /> and <paramref name="purpose" />.
+        /// </returns>
+        public override Task<string> GetUserModifierAsync(string purpose, UserManager<IdCard> manager, IdCard user) =>
+            // enrich entropy with username of the card owner
+            Task.FromResult("Totp:" + purpose + ":" + user.Id + user.CvutUsername);
     }
 }
