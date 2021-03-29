f7a001788d3dc8c24e8b92d89d6f044ff06e022b
Jan Skala <skalaja7@fel.cvut.cz>
2021-03-29 14:40:53 +0200

    Track pages in query parameters.

---
+++ server/SelfServiceLibrary.Web/Extensions/QueryStringParameterExtensions.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Reflection;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Primitives;
+using Microsoft.JSInterop;
+
+using SelfServiceLibrary.Web.Attributes;
+
+namespace SelfServiceLibrary.Web.Extensions
+{
+    public static class QueryStringParameterExtensions
+    {
+        // Apply the values from the query string to the current component
+        public static void SetParametersFromQueryString<T>(this T component, NavigationManager navigationManager)
+            where T : ComponentBase
+        {
+            if (!Uri.TryCreate(navigationManager.Uri, UriKind.RelativeOrAbsolute, out var uri))
+                throw new InvalidOperationException("The current url is not a valid URI. Url: " + navigationManager.Uri);
+
+            // Parse the query string
+            var queryString = QueryHelpers.ParseQuery(uri.Query);
+
+            // Enumerate all properties of the component
+            foreach (var property in GetProperties<T>())
+            {
+                // Get the name of the parameter to read from the query string
+                var parameterName = GetQueryStringParameterName(property);
+                if (parameterName == null)
+                    continue; // The property is not decorated by [QueryStringParameterAttribute]
+
+                if (queryString.TryGetValue(parameterName, out var value))
+                {
+                    // Convert the value from string to the actual property type
+                    var convertedValue = ConvertValue(value, property.PropertyType);
+                    property.SetValue(component, convertedValue);
+                }
+            }
+        }
+
+        private static string BuildNewUri<T>(T component, NavigationManager navigationManager)
+            where T : ComponentBase
+        {
+            if (!Uri.TryCreate(navigationManager.Uri, UriKind.RelativeOrAbsolute, out var uri))
+                throw new InvalidOperationException("The current url is not a valid URI. Url: " + navigationManager.Uri);
+
+            // Fill the dictionary with the parameters of the component
+            Dictionary<string, StringValues> parameters = QueryHelpers.ParseQuery(uri.Query);
+            foreach (var property in GetProperties<T>())
+            {
+                var parameterName = GetQueryStringParameterName(property);
+                if (parameterName == null)
+                    continue;
+
+                var value = property.GetValue(component);
+                if (value is null)
+                {
+                    parameters.Remove(parameterName);
+                }
+                else
+                {
+                    parameters[parameterName] = ConvertToString(value);
+                }
+            }
+
+            // Compute the new URL
+            var newUri = uri.GetComponents(UriComponents.Scheme | UriComponents.Host | UriComponents.Port | UriComponents.Path, UriFormat.UriEscaped);
+            foreach (var parameter in parameters)
+            {
+                foreach (var value in parameter.Value)
+                {
+                    newUri = QueryHelpers.AddQueryString(newUri, parameter.Key, value);
+                }
+            }
+            return newUri;
+        }
+
+        public static ValueTask UpdateQueryString<T>(this T component, NavigationManager navigationManager, IJSRuntime jsRuntime)
+            where T : ComponentBase
+        {
+            var newUri = BuildNewUri(component, navigationManager);
+            return jsRuntime.InvokeVoidAsync("window.history.replaceState", null, "", newUri);
+        }
+
+        // Apply the values from the component to the query string
+        public static void UpdateQueryString<T>(this T component, NavigationManager navigationManager)
+            where T : ComponentBase
+        {
+            var newUri = BuildNewUri(component, navigationManager);
+            navigationManager.NavigateTo(newUri);
+        }
+
+        private static object ConvertValue(StringValues value, Type type) =>
+            Convert.ChangeType(value[0], type, CultureInfo.InvariantCulture);
+
+        private static string? ConvertToString(object value) =>
+            Convert.ToString(value, CultureInfo.InvariantCulture);
+
+        private static PropertyInfo[] GetProperties<T>() =>
+            typeof(T).GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+
+        private static string? GetQueryStringParameterName(PropertyInfo property)
+        {
+            var attribute = property.GetCustomAttribute<QueryStringParameterAttribute>();
+            if (attribute == null)
+                return null;
+
+            return attribute.Name ?? property.Name;
+        }
+    }
+}
