0d37c8bf899b36ee7622d39f903db16e9132c074
Jan Skala <skalaja7@fel.cvut.cz>
2021-04-09 08:42:36 +0200

    Kiosk schema login endpoint.

---
+++ server/SelfServiceLibrary.Web/Pages/LoginKiosk.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using SelfServiceLibrary.BL.Interfaces;
+
+namespace SelfServiceLibrary.Web.Pages
+{
+    [IgnoreAntiforgeryToken]
+    public class LoginKioskModel : PageModel
+    {
+        public async Task OnGet([FromQuery] string redirectUri, [FromQuery] string token)
+        {
+            if (string.IsNullOrEmpty(redirectUri))
+            {
+                redirectUri = "/";
+            }
+
+            // KIOSK login
+            await HttpContext.ChallengeAsync("KIOSK", new AuthenticationProperties { RedirectUri = redirectUri });
+        }
+    }
+}
