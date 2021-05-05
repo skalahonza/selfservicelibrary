a48c29b57603aea88bdacd9df49d11d36c6c3c3c
Jan Skala <skalaja7@fel.cvut.cz>
2021-05-05 08:50:12 +0200

    Add sorting queryable extension test.

---
+++ server/SelfServiceLibrary.Unit.Tests/QueryableExtensions.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+using FluentAssertions;
+
+using SelfServiceLibrary.BL.Extensions;
+using SelfServiceLibrary.DAL.Entities;
+
+using Xunit;
+
+namespace SelfServiceLibrary.Unit.Tests
+{
+    public class QueryableExtensions
+    {
+        [Fact]
+        public void Sort()
+        {
+            // Arrange
+            var books = new List<Book>
+            {
+                new()
+                {
+                    Name = "A",
+                    Author = "John"
+                },
+                new()
+                {
+                    Name = "B",
+                    Author = "John"
+                },
+                new()
+                {
+                    Name = "C",
+                    Author = "Jane"
+                },
+                new()
+                {
+                    Name = "D",
+                    Author = "Jane"
+                },
+            };
+
+            // Act
+            var query = books.AsQueryable().Sort(new[] { ("Author", ListSortDirection.Ascending), ("Name", ListSortDirection.Descending) });
+            var output = query.ToList();
+
+            // Assert
+            output.Should().Equal(
+                books[3],
+                books[2],
+                books[1],
+                books[0]
+            );
+        }
+    }
+}
